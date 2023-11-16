ffe237b1e6c5d0714f19d062d82923d38c37ff15
Vinicius Costa Bezerra <150466641+ViniciusCBezerra@users.noreply.github.com>
2023-11-16 15:27:55 -0300

    atu1

---
+++ ex005_Forca.py
@@ -0,0 +1,9 @@
+def Jogar():
+    print('=-'*20)
+    print('Bem vindo ao jogo da forca!')
+    print('=-'*20)
+
+    print('Fim do jogo')
+
+if __name__ == '__main__':
+    Jogar()
---
+++ ex006_EscolherJogo.py
@@ -0,0 +1,17 @@
+import ex004_Adivinha
+import ex005_Forca
+
+def Jogos():
+    print('=-'*20)
+    print('Escolha um jogo!')
+    print('=-'*20)
+
+    jogo = int(input('Jogo da adivinhação - 1 / Jogo da forca - 2 :'))
+
+    if jogo == 1:
+        ex004_Adivinha.Jogar()
+    elif jogo == 2:
+        ex005_Forca.Jogar()
+
+if __name__ == '__main__':
+    Jogos()

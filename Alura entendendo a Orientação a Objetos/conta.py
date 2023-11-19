fb64616ed82b3f6d16dc37cf6671329c3adb29e9
Vinicius Costa Bezerra <150466641+ViniciusCBezerra@users.noreply.github.com>
2023-11-19 17:55:00 -0300

    atu003

--- Alura entendendo a Orientação a Objetos/conta.py
+++ Alura entendendo a Orientação a Objetos/conta.py
@@ -12,20 +12,54 @@ class Conta:
         self.__saldo +=valor
         print(f'Você {self.__titular} depositou R${valor:.2f} na sua conta')
 
+    def __pode_sacar(self,valor_a_sacar):
+        maximo_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= maximo_sacar
+
     def sacar(self,valor):
-        self.__saldo -=valor
-        print(f'Você {self.__titular} sacou R${valor:.2f} na sua conta')
+        if self.pode_sacar(valor):
+            self.__saldo -= valor
+            print(f'Você {self.__titular} sacou R${valor:.2f} na sua conta')
+        else:
+            print(f'Você não possui o limite para sacar o valor de R${valor}')
+            print(f'O valor maximo é de R${self.__saldo + self.__limite}')
 
     def transferir(self,valor,destino):
         self.sacar(valor)
         destino.depositar(valor)
         print(f'Você {self.__titular} transferiu R${valor:.2f} para {destino._Conta__titular}')
 
-conta = Conta(123,'Vinicius',55.0,1000.0)
-conta2 = Conta(129,'Marcos',150.0,3000.0)
-conta.depositar(100.0)
-conta.extrato()
-conta.sacar(75.0)
-conta.extrato()
+    @property
+    def saldo(self):
+        return self.__saldo
 
-conta2.transferir(50.0,conta)
+    @saldo.setter
+    def saldo(self,novo_saldo):
+        self.__saldo = novo_saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @titular.setter
+    def titular(self,novo_titular):
+        self.__titular = novo_titular
+
+    @property
+    def limite(self):
+        self.__limite
+
+    @limite.setter
+    def limite(self,novo_limite):
+        self.__limite = novo_limite
+
+    @staticmethod
+    def codigo_banco():
+        return '001'
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
+
+codigo = Conta.codigos_bancos()
+print(codigo["BB"])

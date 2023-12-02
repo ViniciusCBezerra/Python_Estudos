6f8b6b49f6df8d4ce389891a1abb28bf09680eac
Vinicius Costa Bezerra <150466641+ViniciusCBezerra@users.noreply.github.com>
2023-12-02 14:24:58 -0300

    atu009

---
+++ 7. Python Brasil validação de dados no padrão nacional/ex004_telefone.py
@@ -0,0 +1,6 @@
+from ex005_telefones_br import TelefoneBr
+
+telefone = '11969173856'
+resultado = TelefoneBr(telefone)
+
+print(resultado)
---
+++ 7. Python Brasil validação de dados no padrão nacional/ex006_datas.py
@@ -0,0 +1,6 @@
+from ex006_datas_br import DatasBr
+from datetime import timedelta,datetime
+
+
+hoje = DatasBr()
+print(hoje.tempo_cadastro())
